96f35ecbcc1da530b40b0084fec24d853a050e32
zishan044 <winchesterfelix007@gmail.com>
2024-02-09 09:22:42 +0600

    taking input a basic diary entry and showing it to console

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Entry struct {
+	title string
+	date  time.Time
+	text  string
+	tags  []string
+}
 
 func main() {
-	fmt.Println("hello world")
+	var newEntry Entry
+
+	fmt.Println("title:")
+	fmt.Scanf("%v", &newEntry.title)
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("entry:")
+	scanned := scanner.Scan()
+	if scanned {
+		newEntry.text = scanner.Text()
+	}
+
+	fmt.Println("tags:")
+	scanned = scanner.Scan()
+	newEntry.tags = strings.Fields(scanner.Text())
+
+	newEntry.date = time.Now()
+
+	fmt.Println()
+
+	fmt.Println(newEntry.title)
+	fmt.Println(newEntry.date)
+	fmt.Println(strings.Join(newEntry.tags, " "))
+	fmt.Println(newEntry.text)
 }
